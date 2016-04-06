1ee608bc21234f7d3e3bfd96b27783b83e076a69
root <o.elumeze@yahoo.com>
2016-04-06 15:01:26 -0400

    All DB SQL Handlers are included

---
+++ app/src/main/java/com/example/root/databasesample/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.root.databasesample;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView idView;
+    EditText productBox;
+    EditText quantityBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container,
+                            new PlaceholderFragment()).commit();
+        }*/
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        idView = (TextView) findViewById(R.id.productID);
+        productBox = (EditText) findViewById(R.id.product_name);
+        quantityBox =
+                (EditText) findViewById(R.id.productQuantity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,
+                menu);
+        return true;
+    }
+
+    public void newProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        int quantity =
+                Integer.parseInt(quantityBox.getText().toString());
+
+        Product product =
+                new Product(productBox.getText().toString(), quantity);
+
+        dbHandler.addProduct(product);
+        productBox.setText("");
+        quantityBox.setText("");
+    }
+
+    public void lookupProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        Product product =
+                dbHandler.findProduct(productBox.getText().toString());
+
+        if (product != null) {
+            idView.setText(String.valueOf(product.getID()));
+
+            quantityBox.setText(String.valueOf(product.getQuantity()));
+        } else {
+            idView.setText("No Match Found");
+        }
+    }
+
+    public void removeProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null,
+                null, 1);
+
+        boolean result = dbHandler.deleteProduct(
+                productBox.getText().toString());
+
+        if (result)
+        {
+            idView.setText("Record Deleted");
+            productBox.setText("");
+            quantityBox.setText("");
+        }
+        else
+            idView.setText("No Match Found");
+    }
+}
---
+++ app/src/main/java/com/example/root/databasesample/PlaceholderFragment.java
@@ -0,0 +1,9 @@
+package com.example.root.databasesample;
+
+import android.app.Fragment;
+
+/**
+ * Created by root on 12/11/15.
+ */
+public class PlaceholderFragment extends Fragment {
+}
